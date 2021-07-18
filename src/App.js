b2c6db33e9f8f98c194cb283c5f72eaeae0812f1
Zaina-coder <81202142+Zaina-coder@users.noreply.github.com>
2021-07-18 13:05:08 -0700

    added API integration

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./shecode.png";
+import './App.css';
+import Dictionary from "./Dictionary"
+
+ export  default function  App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <header className="App-header"> 
+      
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+
+      
+      </header>
+      <main >
+        <Dictionary />
+      </main>
+      <footer className="text-center">
+        coded by Zainab    
+      </footer>
+    </div>
+    </div>
+  );
+}
+
+
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, {useState}from "react";
 import "./Dictionary.css"
+import axios from "axios";
+
 //we store value of searching inside state  beacue the value will change during the lifecyle of component
 
 export default function Dictionary (){
     let[keyword, setkeyword] = useState("");
+   
     // here's where the keyword typed 
     //when user submit the search text it will be store inside the state
+     function handleResponse(response) {
+        console.log(response.data[0]);
+        //adding data to fetch all the array  then [0] to dislpay the first item of the array
+     }
     function handleKeywordChange(event){
         console.log(event.target.value);
         setkeyword(event.target.value);
@@ -15,6 +22,9 @@ export default function Dictionary (){
         event.preventDefault();
         alert(`searching for ${keyword} definition`);
         //shows the value of the state
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        //replace the world hello with whatever word ($keyword) the user type. 
+        axios.get(apiUrl).then(handleResponse);
 
     }
     return (<div className="Dictionary"> 
